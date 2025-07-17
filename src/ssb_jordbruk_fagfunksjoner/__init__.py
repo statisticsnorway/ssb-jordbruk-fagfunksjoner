7d39bea2ec25dd9d03afe5e5b7a5d742194a0807
Magnus Theodor Engh <90612127+MagnusEngh@users.noreply.github.com>
2025-07-17 10:24:00 +0200

    Updates (#56)
    * Update
    
    * Bump version

--- src/ssb_jordbruk_fagfunksjoner/__init__.py
+++ src/ssb_jordbruk_fagfunksjoner/__init__.py
@@ -1,1 +1,11 @@
 """SSB Jordbruk Fagfunksjoner."""
+
+from .produksjonstilskudd import VALID_MEASUREMENT_UNITS
+from .produksjonstilskudd import Produksjonskode
+from .produksjonstilskudd import Produksjonstilskudd
+
+__all__ = [
+    "VALID_MEASUREMENT_UNITS",
+    "Produksjonskode",
+    "Produksjonstilskudd",
+]
